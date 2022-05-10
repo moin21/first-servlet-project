4fe8e7bd8cd98b0d4e37fc5b76821091e700185f
moin21 <mamoinuddin@gmail.com>
2022-05-11 01:19:26 +0530

    Added: Logic to validate name

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,13 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
         urlPatterns = {"/LoginServlet"},
 
         // Setting a couple of initial parameter for the servlet
-        initParams = { @WebInitParam(name = "user", value = "Tom"),
+        initParams = { @WebInitParam(name = "user", value = "Moin"),
                 @WebInitParam(name = "password", value = "Bridgelabz@123") })
 
 public class LoginServlet extends HttpServlet {
@@ -29,11 +31,22 @@ public class LoginServlet extends HttpServlet {
         String user = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
 
+        Pattern pattern = Pattern.compile("^([A-Z][a-zA-Z]{2,}[ ]?)+$");
+        Matcher matcher = pattern.matcher(userInput);
+        if(!matcher.matches()) {
+            PrintWriter out = response.getWriter();
+            out.println("<font color = red> Incorrect UserId. Please try again!!<font>");
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            rd.include(request, response);
+            return;
+        }
+
         if (user.equals(userInput) && password.equals(pwdInput)) {
-            System.out.println(" Correct user");
             request.setAttribute("user", user);
             request.getRequestDispatcher("/LoginSuccess.jsp").forward(request, response);
-        } else {
+        }
+
+        else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
             out.println("<font color = red> Incorrect UserId or Password. Please try again!!<font>");
